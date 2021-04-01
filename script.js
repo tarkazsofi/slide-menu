7f41aaa219e434a9688dab35b1d0ab3261d43b97
Zsofia Tarkanyi <zsofi.t@gmail.com>
2021-04-01 22:25:59 +0200

    Hid and showed side menu

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
+  menu.classList.toggle("open");
 });
